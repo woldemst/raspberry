36f7a3902a47367d19bc177f5c9690e74f7edede
woldemst <72108511+woldemst@users.noreply.github.com>
2023-04-27 11:13:33 +0200

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, Response
 import cv2
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-def generate_frames():
-    camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
+def gen_frames():
     while True:
         success, frame = camera.read()
-
         if not success:
             break
         else:
@@ -17,15 +16,13 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    camera.release()
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello, World!"
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8000, debug=True)
